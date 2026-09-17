f0fb0d32414d9fcec544be862b188226dbf3f44f
theosmith0308 <theosmith0308@outlook.com>
2026-09-17 14:12:29 +0200

    Changes

--- plugins.zsh
+++ plugins.zsh
@@ -25,5 +25,4 @@ zplugin-update() {
 
 _zplugin_load zsh-users zsh-autosuggestions
 _zplugin_load zsh-users zsh-history-substring-search
-_zplugin_load jeffreytse zsh-vi-mode
 _zplugin_load zdharma-continuum fast-syntax-highlighting
